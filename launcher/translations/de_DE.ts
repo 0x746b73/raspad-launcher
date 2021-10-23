133a53b18945ecc6c4c493d19f74007f97b47f0e
purewasa <48826305+purewasa@users.noreply.github.com>
2021-10-23 22:20:45 +0200

    Add German translation

---
+++ launcher/translations/de_DE.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="62"/>
+        <source>Network Error</source>
+        <translation>Netzwerkfehler</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="95"/>
+        <source>Home</source>
+        <translation>Start</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="100"/>
+        <source>Programming</source>
+        <translation>Entwicklung</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="105"/>
+        <source>Education</source>
+        <translation>Bildung</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="110"/>
+        <source>Office</source>
+        <translation>Büro</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="115"/>
+        <source>Internet</source>
+        <translation>Internet</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="120"/>
+        <source>Sound &amp; Video</source>
+        <translation>Unterhaltungsmedien</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="125"/>
+        <source>Graphics</source>
+        <translation>Grafik</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="130"/>
+        <source>Games</source>
+        <translation>Spiele</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="135"/>
+        <source>Accessories</source>
+        <translation>Zubehör</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="140"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="145"/>
+        <source>Preferences</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="150"/>
+        <source>Run</source>
+        <translation>Ausführen</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="268"/>
+        <source>Recommend:</source>
+        <translation>Empfohlen:</translation>
+    </message>
+</context>
+<context>
+    <name>run</name>
+    <message>
+        <location filename="../qml/run.qml" line="23"/>
+        <source>Enter the command you want to execute: </source>
+        <translation>Geben Sie den Befehl ein, den Sie ausführen wollen: </translation>
+    </message>
+</context>
+</TS>
